ddd76c25184daa78757dcb263da4905fa943d05c
Brian Gershon <brian.five@gmail.com>
2015-01-27 04:41:42 -0800

    Fix css for 'notice' msg. Validate :name field.

--- app/models/pixel_pic.rb
+++ app/models/pixel_pic.rb
@@ -1,4 +1,4 @@
 class PixelPic < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, format: { with: /\A[a-z0-9]+\z/, message: "lower case letters and numbers only" }
   validates :image_characters, uniqueness: true, presence: true, length: { is: 64 }, format: { with: /\A[orgy]+\z/, message: "only allows 'o' for empty, 'r' for red, 'g' for green, 'y' for yellow." }
 end
